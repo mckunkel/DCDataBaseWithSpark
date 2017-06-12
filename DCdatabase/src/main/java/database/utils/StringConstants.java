a871ae8e6a1ccd179eb84b89041f72dbe0e2b495
mckunkel <mkunkel@jlab.org>
2017-06-12 12:51:28 +0200

    uploading homework

--- DCdatabase/src/main/java/database/utils/StringConstants.java
+++ DCdatabase/src/main/java/database/utils/StringConstants.java
@@ -21,7 +21,9 @@ public class StringConstants {
 	// things for spark
 	public static final String APP_NAME = "DC Database Management Application";
 	public static final String TEMP_DIR = "spark-warehouse";
-	public static final String SPARK_MASTER = "local[2]";
+	public static final String SPARK_MASTER = "local[2]";//
+															// "spark://Mike-Kunkels-MacBook.local:7077";//
+	// "local[2]";
 
 	// things for menu
 	public static final String MAIN_MENU_FILE = "File";
