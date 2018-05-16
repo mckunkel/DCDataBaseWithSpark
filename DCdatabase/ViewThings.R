191dca953e80eedcae1e77756b447e76ade02c54
mckunkel <mkunkel@jlab.org>
2018-05-16 19:50:17 +0200

    done with cleaning...now ready for betta testing

--- DCdatabase/ViewThings.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(data.table)
-library(ggthemes)
-
-df <- read.table("/Users/michaelkunkel/WORK/GIT_HUB/DCDataBaseWithSpark/DCdatabase/Run_3432.txt", header = FALSE)
-head(df)
-subdf<- subset(df,V4!=0)
-pl<-ggplot(subdf,aes(x=V4))+geom_histogram()
-pl
